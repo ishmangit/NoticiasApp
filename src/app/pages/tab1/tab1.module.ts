50646237bc82d00d0b8bb1b152418cdfd60ea176
ishman <ishman99@gmail.com>
2019-03-26 18:31:56 +0100

    Contenido del tab sacado a componentes

--- src/app/pages/tab1/tab1.module.ts
+++ src/app/pages/tab1/tab1.module.ts
@@ -1,8 +1,9 @@
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponentsModule } from './../../components/components.module';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Tab1Page } from './tab1.page';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { Tab1Page } from './tab1.page';
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{ path: '', component: Tab1Page }]),
+    ComponentsModule
   ],
   declarations: [Tab1Page]
 })
