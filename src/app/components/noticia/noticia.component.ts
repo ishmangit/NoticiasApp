31305a88e8ad83ef76664680488088255c260d41
ishman <ishman99@gmail.com>
2019-03-28 09:49:49 +0100

    Implementado diseño de action sheet noticia

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { ActionSheetController } from '@ionic/angular';
 import { Article } from './../../interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
@@ -13,12 +14,45 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() index: number;
 
-  constructor(private iab: InAppBrowser) { }
+  constructor(private iab: InAppBrowser,
+              private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {}
 
   abrirNoticia() {
     const browser = this.iab.create(this.noticia.url, '_system');
+  }
+
+  async lanzarMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share',
+        cssClass: 'actionDark',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      },
+      {
+        text: 'Guardar en favoritos',
+        icon: 'star',
+        cssClass: 'actionDark',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      },
+      {
+        text: 'Cancelar',
+        icon: 'close',
+        cssClass: 'actionDark',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+
+    await actionSheet.present();
   }
 
 }
